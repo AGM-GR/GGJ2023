0288056e3c4ddbd26b8864ab751c338a930ced3b
Alejandro Guerrero Martínez <alejandroguerrero.gr@gmail.com>
2023-02-04 18:26:23 +0100

    Ravers continous spawn

--- Assets/Scripts/Raver/Raver.cs
+++ Assets/Scripts/Raver/Raver.cs
@@ -10,7 +10,10 @@ public class Raver : MonoBehaviour
     [SerializeField]
     private RaverMaterials _raverMaterials;
 
+    private Vector3 _finalDestination;
+
     public NavMeshAgent NavMeshAgent => _navMeshAgent;
+    public RaversSpawner RaverSpawner { get; set;}
 
     private void OnValidate()
     {
@@ -24,6 +27,31 @@ public class Raver : MonoBehaviour
     public void SetDestination(Vector3 destination)
     {
         _navMeshAgent.SetDestination(destination);
+    }
+
+    public void SetExitDestination(Vector3 destination)
+    {
+        _finalDestination = destination;
+    }
+
+    public void StartRaverLogic()
+    {
+        gameObject.SetActive(true);
+        _navMeshAgent.enabled = true;
+        StartCoroutine(RaverLogic());
+    }
+
+    private IEnumerator RaverLogic()
+    {
+        yield return new WaitForSeconds(Random.Range(0.2f, 1f));
+        SetDestination(_finalDestination);
+    }
+
+    public void DisableRaver()
+    {
+        RaverSpawner.RaverDisabled();
+        _navMeshAgent.enabled = false;
+        gameObject.SetActive(false);
     }
 
     public void InfluencedByPlayer(RaveColor raveColor, Vector3 destination)
--- Assets/Scripts/Raver/RaversSpawner.cs
+++ Assets/Scripts/Raver/RaversSpawner.cs
@@ -4,24 +4,46 @@ using UnityEngine;
 public class RaversSpawner : NavMeshSpawner<Raver>
 {
     [Header("Ravers Spawner")]
-    public int _initializeRaverCount = 20;
+    public int _maxRaverCount = 100;
+    public int _raversSpawnBatch = 10;
     public float _spawnRatio = 0.1f;
-    public Renderer[] exitAreas;
+    public Renderer[] _exitAreas;
+
+    private int _totalActiveRavers = 0;
 
     private IEnumerator Start()
     {
-        for (int i=0; i < _initializeRaverCount; i++)
+        while(true)
         {
-            Raver raverSpawned = SpawnRandom();
-            if (raverSpawned != null)
+            if (_totalActiveRavers < _maxRaverCount)
             {
-                yield return new WaitForSeconds(_spawnRatio);
-                Vector3 exitPoint = Utils.GetRandomPointInPlane(exitAreas[Random.Range(0, exitAreas.Length)]);
-                raverSpawned.SetDestination(exitPoint);
-            }
+                for (int i = 0; i < _raversSpawnBatch; i++)
+                {
+                    Raver raverSpawned = SpawnRandom();
+                    if (raverSpawned != null)
+                    {
+                        yield return new WaitForSeconds(_spawnRatio);
+                        Vector3 exitPoint = Utils.GetRandomPointInPlane(_exitAreas[Random.Range(0, _exitAreas.Length)]);
 
-            yield return null;
+                        raverSpawned.RaverSpawner = this;
+                        raverSpawned.SetExitDestination(exitPoint);
+                        raverSpawned.StartRaverLogic();
+                        _totalActiveRavers++;
+                    }
+                }
+
+                yield return new WaitForSeconds(_spawnRatio);
+            } 
+            else
+            {
+                yield return null;
+            }
         }
+    }
+
+    public void RaverDisabled()
+    {
+        _totalActiveRavers--;
     }
 
 }
