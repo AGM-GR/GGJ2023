4772e2b5271a3d18c90a6e786b5de88d9e511a23
Kiko <kikoalfaro2@gmail.com>
2023-02-04 03:01:14 +0100

    WIP - Roles

---
+++ Assets/Code/Role.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+public abstract class Role
+{
+    public float Radius = 1;
+    public abstract string TargetTag { get; }
+
+    public abstract void ExecuteAction();
+}
+
+public class PR : Role
+{
+    public override string TargetTag => "Player";
+
+    public override void ExecuteAction()
+    {
+        // si tiene un grupo en rango, les hace ir hacia el coche propio
+        Debug.Log("Execute PR action");
+    }
+}
+
+public class Badass : Role
+{
+    public override string TargetTag => "Player";
+
+    public override void ExecuteAction()
+    {
+        // si está cerca del PR o del coche enemigo, lincharle
+        Debug.Log("Execute Badass action");
+    }
+}
+
+public class Dj : Role
+{
+    public override string TargetTag => "Player";
+
+    public override void ExecuteAction()
+    {
+        // si está cerca del coche, empezar minijuego influencia
+        Debug.Log("Execute Dj action");
+    }
+}
---
+++ Assets/Code/RoleController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RoleController : MonoBehaviour
+{
+    private Role _currentRole;
+    private int _currentRoleIndex;
+    private Role[] _roles = new Role[3];
+    private PlayerActions_ _playerActions;
+
+    public SphereCollider SphereCollider;
+    private bool _canExecuteAction;
+
+    private void Awake()
+    {
+        _playerActions = new PlayerActions_();
+        _playerActions.Enable();
+
+        _playerActions.Player.ExecuteRoleAction.performed += ctx => ExecuteRoleAction(ctx);
+        _playerActions.Player.SwitchRole.performed += ctx => SwitchRole(ctx);
+
+        SphereCollider.isTrigger = true;
+
+        _roles[0] = new PR();
+        _roles[1] = new Badass();
+        _roles[2] = new Dj();
+
+        _currentRole = _roles[0];
+    }
+
+
+    public void SwitchRole(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Switch role");
+
+        _currentRoleIndex++;
+        if(_currentRoleIndex >= _roles.Length) _currentRoleIndex = 0;
+
+        _currentRole = _roles[_currentRoleIndex];
+        SphereCollider.radius = _currentRole.Radius;
+    }
+
+    public void ExecuteRoleAction(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Execute action");
+
+        if (_canExecuteAction)
+        {
+            _currentRole.ExecuteAction();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _playerActions.Disable();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+            Debug.Log("trigger");
+        if (other.CompareTag(_currentRole.TargetTag))
+        {
+            _canExecuteAction = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _canExecuteAction = false;
+    }
+}
